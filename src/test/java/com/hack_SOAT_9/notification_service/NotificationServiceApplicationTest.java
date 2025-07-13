9fa272a1a18dcc53e06a06fd8c78bb7433a0008f
larissamy <74251920+larissamy@users.noreply.github.com>
2025-07-13 17:57:15 -0300

    feat: add main method and enable caching in NotificationServiceApplicationTest

--- src/test/java/com/hack_SOAT_9/notification_service/NotificationServiceApplicationTest.java
+++ src/test/java/com/hack_SOAT_9/notification_service/NotificationServiceApplicationTest.java
@@ -1,4 +1,13 @@
 package com.hack_SOAT_9.notification_service;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+@SpringBootApplication
+@EnableCaching
 public class NotificationServiceApplicationTest {
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationServiceApplicationTest.class, args);
+    }
 }
